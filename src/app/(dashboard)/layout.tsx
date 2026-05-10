e16517ac1d5869a3e4d4f9f32460a4ddcaa93698
KbDevs12 <daniaku203@gmail.com>
2026-05-11 00:31:52 +0700

    chore: re-design layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,16 +9,16 @@ export default function DashboardLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className="flex min-h-screen">
+      <div className="flex min-h-screen w-full">
         <DashboardSidebar />
 
-        <main className="flex-1 w-full">
+        <main className="flex-1 min-w-0 w-full">
           <div className="flex items-center gap-3 p-2 border-b">
             <SidebarTrigger />
             <DashboardBreadcrumb />
           </div>
 
-          <div className="p-6">{children}</div>
+          <div className="p-6 w-full">{children}</div>
         </main>
       </div>
     </SidebarProvider>
