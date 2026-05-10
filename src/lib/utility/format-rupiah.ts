a4f9c18fa8e115aa22cd5826cb1511b6b34c9f03
KbDevs12 <daniaku203@gmail.com>
2026-05-10 18:07:12 +0700

    chore: create new rupiah format

--- src/lib/utility/format-rupiah.ts
+++ src/lib/utility/format-rupiah.ts
@@ -4,3 +4,13 @@ export function FormatRupiah(amount: number): string {
     currency: "IDR",
   }).format(amount);
 }
+
+export function FormatRupiahV2(amount: number): string {
+  if (amount >= 1_000_000) {
+    return `${(amount / 1_000_000).toFixed(1).replace(".0", "")}jt`;
+  }
+  if (amount >= 1_000) {
+    return `${(amount / 1_000).toFixed(1).replace(".0", "")}rb`;
+  }
+  return amount.toString();
+}
