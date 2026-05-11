fb64b6356968b3f50e728e97f971333aa9ba0ed2
KbDevs12 <daniaku203@gmail.com>
2026-05-11 11:03:34 +0700

    chore: updating sidebar types

--- src/types/sidebar.ts
+++ src/types/sidebar.ts
@@ -1,8 +1,11 @@
 import { LucideIcon } from "lucide-react";
 
+export type DashboardRole = "admin" | "superadmin";
+
 export interface SidebarMenu {
   title: string;
   icon?: LucideIcon;
   path?: string;
+  roles?: DashboardRole[];
   subMenu?: SidebarMenu[];
 }
